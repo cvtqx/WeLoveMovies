3acf540b03dfe0dc0020f4cc2ebdebcae6bcc57b
Olga Yudkin <cvtqx@protonmail.com>
2022-12-29 11:01:21 -0500

    initial commit

--- knexfile.js
+++ knexfile.js
@@ -3,13 +3,20 @@ const path = require("path");
 require("dotenv").config();
 
 const {
-  DATABASE_URL = "postgres://dmbwlils:So9epvFdjiCXOf0xo3e_2g0wDVjvxkVt@berry.db.elephantsql.com/dmbwlils",
+  NODE_ENV = "development",
+  DEVELOPMENT_DATABASE_URL,
+  PRODUCTION_DATABASE_URL,
 } = process.env;
+
+const URL =
+NODE_ENV === "production"
+  ? PRODUCTION_DATABASE_URL
+  : DEVELOPMENT_DATABASE_URL;
 
 module.exports = {
   development: {
     client: "postgresql",
-    connection: DATABASE_URL,
+    connection: URL,
     pool: { min: 0, max: 5 },
     migrations: {
       directory: path.join(__dirname, "src", "db", "migrations"),
@@ -21,7 +28,7 @@ module.exports = {
 
   production: {
     client: "postgresql",
-    connection: DATABASE_URL,
+    connection: URL,
     pool: { min: 0, max: 5 },
     migrations: {
       directory: path.join(__dirname, "src", "db", "migrations"),
